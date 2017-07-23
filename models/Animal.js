b142e2c9fdd9d939ab735932cb6bb202bf1ad5a4
Dmitrii Olkhov <reminder63@gmail.com>
2017-07-23 19:04:04 +0400

    Added two base models

---
+++ models/Animal.js
@@ -0,0 +1,19 @@
+var mongoose = required('mongoose');
+
+var AnimalSchema = mongoose.Schema({
+	name: {
+		type: String,
+		uniquie: true,
+		required: true	
+	}
+	kind: String,
+	age: Number,
+	nourished_at: Date,
+	cage: String,
+	keeper: [{
+		first_name: String,
+		last_name: String
+	}]
+});
+
+module.exports = mongoose.model('Animal', AnimalSchema);
